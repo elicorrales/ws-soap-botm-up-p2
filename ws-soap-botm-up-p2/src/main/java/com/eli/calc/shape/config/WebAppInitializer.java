7e18e2fc88c24ee59e5367c4ea5a7899d062ed7b
elicorrales <e_corrales2003@yahoo.com>
2016-09-21 01:04:39 -0400

    shutdown

--- ws-soap-botm-up-p2/src/main/java/com/eli/calc/shape/config/WebAppInitializer.java
+++ ws-soap-botm-up-p2/src/main/java/com/eli/calc/shape/config/WebAppInitializer.java
@@ -11,6 +11,8 @@ import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
+import com.eli.calc.shape.listeners.impl.ShapeCalcServletContextListener;
+
 
 public class WebAppInitializer implements WebApplicationInitializer {
 
@@ -35,9 +37,9 @@ public class WebAppInitializer implements WebApplicationInitializer {
 		logger.debug("\n\n\n\nELI: Web AppInitializer add root context loader listener to servletcontext..... \n\n\n\n");
 		servletContext.addListener(loaderListener);
 
-		//logger.debug("\n\n\n\nELI: Web AppInitializer add shape calc servlet context listener to servletcontext..... \n\n\n\n");
-		//ShapeCalcServletContextListener myServletContextListener = new ShapeCalcServletContextListener(rootCtx);
-		//servletContext.addListener(myServletContextListener);
+		logger.debug("\n\n\n\nELI: Web AppInitializer add shape calc servlet context listener to servletcontext..... \n\n\n\n");
+		ShapeCalcServletContextListener myServletContextListener = new ShapeCalcServletContextListener(rootCtx);
+		servletContext.addListener(myServletContextListener);
 
 		//logger.debug("\n\n\n\nELI: Web AppInitializer created dispatcher servlet passing root ctx..... \n\n\n\n");
 		//DispatcherServlet dispatcherServlet = new DispatcherServlet(rootCtx);
